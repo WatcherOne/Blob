7ce7639776fc1ff247ffa26b382b86f018d0f60b
zhubo <286154864@qq.com>
2022-06-07 11:42:01 +0800

    add session demo

--- middlewares/common.js
+++ middlewares/common.js
@@ -5,7 +5,7 @@ import AjaxResult from '../java/model/ajaxResult.js'
 export const checkIsLogin = async (ctx, next) => {
     if (ctx.query.id) {
         ctx.body = new AjaxResult().set401()
-    } else {
-        return next()
+        return
     }
+    await next()
 }
